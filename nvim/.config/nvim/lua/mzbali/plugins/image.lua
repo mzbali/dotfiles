f38f86ad7b6ae56cee21bb562b35bba7bf3df97b
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2026-06-04 23:42:12 +0100

    Fix image.nvim inside tmux

--- nvim/.config/nvim/lua/mzbali/plugins/image.lua
+++ nvim/.config/nvim/lua/mzbali/plugins/image.lua
@@ -1,7 +1,12 @@
 return {
     "3rd/image.nvim",
+    ft = { "markdown" },
     build = false,
+    cond = function()
+        return #vim.api.nvim_list_uis() > 0
+    end,
     opts = {
+        backend = "kitty",
         processor = "magick_cli",
     },
 }
